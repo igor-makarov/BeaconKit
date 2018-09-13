85e32dec79aaab272dfbdec52c32d81c11d87853
Igor Makarov <igormaka@gmail.com>
2018-09-13 11:44:30 +0300

    switch to SwiftPM 4.0

--- Package.swift
+++ Package.swift
@@ -1,8 +1,20 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
     name: "BeaconKit",
+    products: [
+        .library(
+            name: "BeaconKit",
+            targets: ["BeaconKit"])
+        ],
+    targets: [
+        .target(name: "BeaconKit"),
+        .testTarget(name: "BeaconKitTests",
+                    dependencies: [
+                        "BeaconKit"
+            ])
+        ],
     swiftLanguageVersions: [3, 4]
 )
