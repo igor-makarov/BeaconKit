f98d214b786d313af2b7c15f47833ad630f4f93a
Igor Makarov <igormaka@gmail.com>
2018-12-31 09:43:55 +0200

    fix SwiftLint

--- Scripts/ci_phases/01_lint_project.sh
+++ Scripts/ci_phases/01_lint_project.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l -x
 set +o pipefail # ugh, this kept linter from reporting fails
-! (Pods/SwiftLint/swiftlint --reporter emoji 2> /dev/null | tee /dev/fd/2 | grep "⛔️" > /dev/null)
+! (swiftlint --reporter emoji 2> /dev/null | tee /dev/fd/2 | grep "⛔️" > /dev/null)
